715eb8ff33b15acc5508f30b396669d0dc710c30
FabienMarti <martiretti.fabien@gmail.com>
2020-05-21 21:50:07 +0200

    commit 2 update JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
 class Set {
-    constructor(name, set, rep, timer) {
-        this.name = name;
+    constructor(title, set, rep, timer) {
+        this.title = title;
         this.set = set;
         this.rep = rep;
         this.timer = timer;
     }
 }
-
-
+var setArray = [];
+var setNumber = 0;
+function showSets() {
+    document.getElementById('arrayIncrement').innerHTML = '';
+    for (let index = 0; index < setArray.length; index++) {
+        var add = document.createElement('tr');
+        add.innerHTML =
+            '<th scope="row">' + setArray[index].title + '</th>' +
+            '<td>' + setArray[index].set + '</td>' +
+            '<td>' + setArray[index].rep + '</td>' +
+            '<td>' + setArray[index].timer + ' secondes' + '</td>';
+        document.getElementById('arrayIncrement').appendChild(add);
+    }
+}
+function newSet(title, sets, reps, timer) {
+    let setName = new Set(title , sets, reps, timer);
+    setArray.push(setName);
+    console.log(setArray);
+    showSets();
+}
 
